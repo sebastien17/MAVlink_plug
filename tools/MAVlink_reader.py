03ee29d5b85dbb0391f1878569e05da83209652a
sebastien17 <sebastien.lautier@gmail.com>
2014-09-23 12:36:44 -0400

    Added tool to subscribe to Publisher
    Simple example tool

---
+++ tools/MAVlink_reader.py
@@ -0,0 +1,23 @@
+import sys
+import zmq
+
+port = "42017"
+
+# Socket to talk to server
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+
+print "Collecting updates from MAVlink_plug..."
+socket.connect ("tcp://localhost:%s" % port)
+
+
+# Subscribe to zipcode, default is NYC, 10001
+topicfilter = "HEARTBEAT"
+socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
+
+# Process 5 updates
+total_value = 0
+while True:
+    string = socket.recv()                      #Blocking
+    topic, messagedata = string.split(" ",1)
+    print topic, messagedata
