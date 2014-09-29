8dd66900abc95e2abd8d501d4632c5a01a3ea0a7
sebastien17 <sebastien.lautier@gmail.com>
2014-09-29 17:28:23 -0400

    Fly to browser location when load the html interface

--- tools/html_interface/js/html_interface.js
+++ tools/html_interface/js/html_interface.js
@@ -1,34 +1,49 @@
 $( document ).ready(function() {
     init_fi();
     init_event();
-    var viewer = new Cesium.Viewer('cesiumContainer');
+    init_cesiumjs();    
 });
+
+function init_cesiumjs(){
+    //Init cesiumjs viewer
+    var viewer = new Cesium.Viewer('cesiumContainer');
+    var scene = viewer.scene;
+    // Ask browser for location, and fly there.
+    navigator.geolocation.getCurrentPosition(fly);
+    
+    // Create callback for browser's geolocation
+    function fly(position) {
+    scene.camera.flyTo({
+        destination : Cesium.Cartesian3.fromDegrees(position.coords.longitude, position.coords.latitude, 1000.0)
+        });
+    };
+}
     
 function init_fi(){
-        var ajax_port = 43017;
-        var options = {
-            size : 200,             // Sets the size in pixels of the indicator (square)
-            showBox : false,         // Sets if the outer squared box is visible or not (true or false)
-            img_directory : 'fi/img/'  // The directory where the images are saved to
-        }
-        //Initializing FI 
-        var attitude = $.flightIndicator('#attitude', 'attitude', options);
-        var heading = $.flightIndicator('#heading', 'heading', options);
-        //Setting FI updates
-        setInterval(function() {
-            $.ajax({
-                url : 'http://127.0.0.1:'+ ajax_port, 
-                type : 'POST', 
-                data : { topic: "ATTITUDE", type: "GET_VALUE"},
-                dataType : 'json',
-                success : function(data, status){
-                    attitude.setRoll(-data['roll']/Math.PI*180);
-                    attitude.setPitch(data['pitch']/Math.PI*180);
-                    heading.setHeading(data['yaw']/Math.PI*180);
-                }
-            });
-        }, 50);
+    var ajax_port = 43017;
+    var options = {
+        size : 200,             // Sets the size in pixels of the indicator (square)
+        showBox : false,         // Sets if the outer squared box is visible or not (true or false)
+        img_directory : 'fi/img/'  // The directory where the images are saved to
     }
+    //Initializing FI 
+    var attitude = $.flightIndicator('#attitude', 'attitude', options);
+    var heading = $.flightIndicator('#heading', 'heading', options);
+    //Setting FI updates
+    setInterval(function() {
+        $.ajax({
+            url : 'http://127.0.0.1:'+ ajax_port, 
+            type : 'POST', 
+            data : { topic: "ATTITUDE", type: "GET_VALUE"},
+            dataType : 'json',
+            success : function(data, status){
+                attitude.setRoll(-data['roll']/Math.PI*180);
+                attitude.setPitch(data['pitch']/Math.PI*180);
+                heading.setHeading(data['yaw']/Math.PI*180);
+            }
+        });
+    }, 50);
+}
 
     
 function init_event(){
