4ec46347e8d6248965e0e26e10bffec9f7391d16
sebastien17 <sebastien.lautier@gmail.com>
2015-05-30 17:51:56 +0200

    QuadCopter class completed

---
+++ example/zmq_read.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#	-*- coding: utf-8 -*-
+
+import zmq, time
+
+_PORT = 'tcp://127.0.0.1:45064'
+
+if(__name__ == '__main__'):
+    _context = zmq.Context()
+    socket = _context.socket(zmq.SUB)
+    socket.bind(_PORT)
+    socket.setsockopt(zmq.SUBSCRIBE, ''.encode())
+    while(True):
+        string = socket.recv()
+        print(string)
--- mavlinkplug/QuadCopter.py
+++ mavlinkplug/QuadCopter.py
@@ -25,42 +25,54 @@ from pyfdm import fdmexec
 from pyfdm.exchange import zmq_exchange
 from zmq import Context
 from json import loads
+from os import path, sep
+
+JSBSIM_DEFAULT_PATH = path.dirname(__file__)
 
 class QuadCopter(multiprocessing.Process):
-    def __init__(self, zmq_context = None, zmq_in = None, zmq_out = None, lat = 0, long = 0, terrain_elev_ft = 0, h_agl_ft = 1, fdm_model = 'QuadCopter', jsbsim_root = None, dt = 1.0/100):
-         super(QuadCopter,self).__init__()
+    
+    _data_in = [ 
+    'fcs/throttle-cmd-norm[0]',
+    'fcs/throttle-cmd-norm[1]',
+    'fcs/throttle-cmd-norm[2]',
+    'fcs/throttle-cmd-norm[3]'
+    ]
+
+    _data_out = [
+    #for HIL_GPS 
+    'position/lat-gc-rad',
+    'position/long-gc-rad',
+    'position/h-sl-ft',
+    #for HIL_SENSOR
+    #Need magnetic field composant
+    'accelerations/udot-ft_sec2',
+    'accelerations/vdot-ft_sec2',
+    'accelerations/wdot-ft_sec2',
+    'velocities/p-aero-rad_sec',
+    'velocities/q-aero-rad_sec',
+    'velocities/r-aero-rad_sec',
+    'atmosphere/pressure-altitude',
+    ]
+    
+    JSBSIM_DEFAULT_PATH = path.dirname(__file__) + sep + 'data' + sep
+    
+    def __init__(self, zmq_context = None, zmq_in = None, zmq_out = None, daemon = True, lat = 0, long = 0, terrain_elev_ft = 0, h_agl_ft = 1, fdm_model = 'arducopter', jsbsim_root = None, dt = 1.0/100):
+        super(QuadCopter,self).__init__()
         self._zmq_context = zmq_context
         self._zmq_in = zmq_in
         self._zmq_out = zmq_out
+        self.daemon = daemon
         self._lat_rad = lat
         self._long_rad = long
         self._terrain_elev_ft = terrain_elev_ft
         self._h_agl_ft = h_agl_ft
         self._fdm_model = fdm_model
-        self._jsbsim_root = jsbsim_root
+        if(jsbsim_root != None):
+            self._jsbsim_root = jsbsim_root
+        else:
+            self._jsbsim_root = self.JSBSIM_DEFAULT_PATH
         self._dt = dt
-        self._data_in = [ 
-            'fcs/throttle-cmd-norm[0]',
-            'fcs/throttle-cmd-norm[1]',
-            'fcs/throttle-cmd-norm[2]',
-            'fcs/throttle-cmd-norm[3]'
-            ]
 
-        self._data_out = [
-            #for HIL_GPS 
-            'position/lat-gc-rad',
-            'position/long-gc-rad',
-            'position/h-sl-ft',
-            #for HIL_SENSOR
-            #Need magnetic field composant
-            'accelerations/udot-ft_sec2',
-            'accelerations/vdot-ft_sec2',
-            'accelerations/wdot-ft_sec2',
-            'velocities/p-aero-rad_sec',
-            'velocities/q-aero-rad_sec',
-            'velocities/r-aero-rad_sec',
-            'atmosphere/pressure-altitude',
-            ]
     def setup(self):
         #Flight Loop setup
         self._fdm = fdmexec.FGFDMExec(root_dir=self._jsbsim_root)
@@ -81,9 +93,10 @@ class QuadCopter(multiprocessing.Process):
         self.setup()
         self._fdm.realtime(self._dt)
     def stop(self):
-        self._fdm.stop_realtime() #Not sure it will work
-    @staticmethod
-    def mav_2_FL(string):
+        self.terminate()
+    
+    @classmethod
+    def mav_2_FL(cls, string):
         topic, _timestamp, messagedata = string.split(" ",2)
         data = loads(messagedata)
         if('SERVO_OUTPUT_RAW' in data):
@@ -94,6 +107,14 @@ class QuadCopter(multiprocessing.Process):
                     )
         else:
             return None
-    @staticmethod
-    def FL_2_mav(string):
-        pass
+   
+    @classmethod
+    def FL_2_mav(cls, string):
+        _temp = []
+        _messagedata = string.split(" ")
+        for index, value in item(_messagedata):
+            temp[cls._data_out[index]] = float(value)
+        return _temp
+
+if(__name__ == '__main__'):
+    pass
