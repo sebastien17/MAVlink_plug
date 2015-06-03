219ca4ff28d2e8de1e405c318e81262b5c04a5f7
Sebastien <sebastien.lautier@gmail.com>
2015-06-03 09:03:54 +0200

    Add Exception and PlugMessage class

--- mavlinkplug/Message.py
+++ mavlinkplug/Message.py
@@ -23,43 +23,70 @@ import pymavlink.dialects.v10.pixhawk as mavlink
 import struct
 
 #TODO : Message Type definition
+MSG_PLUG_ID_MAV_MSG=1
 
+MSG_PLUG_TYPE = {
+    MSG_PLUG_ID_MAV_MSG : MAVlinkPlugMessage
+    }
 #TODO : Special Destination, Source Definition
+from mavlinkplug.Exception import MAVlinkPlugException 
 
 class PlugMessage(object):
     
-    def __init__(self, mav_msg_buf, plug_header = False):
-        self._dest = None
-        self._sour = None
-        self._type = None
-        if(plug_header):
-            self._mav_msg_buf = mav_msg_buf[4:]
+    def __init__(self, msg = None):
+        if(msg == None):
+            self._dest = None
+            self._sour = None
+            self._type = None
+            self._mav_msg_data = None
         else:
-            self._mav_msg_buf = mav_msg_buf
-        self._mav_msg_data = None
+            self._dest, self._sour, self._type  = struct.unpack('!BBB',msg)
+            self._mav_msg_data = msg[3:]
+        
+    def set_header(self,dest, type, source):
+        self._dest = dest
+        self._sour = type
+        self._type = source
+        self._check_header()
     
-    def header(self, dest, type, source):
-        if(isinstance(dest, int) and isinstance(type, int) and isinstance(source, int)): 
-            self._dest = dest
-            self._sour = type
-            Self._type = source
+    def _check_header(self):
+        #Destination check
+        if( not isinstance(self._dest, int) or self._dest < 0 or self._dest > 255):
+            raise MAVlinkPlugException('Invalid header data : destination : {0}'.format(self._dest))
+        #Source check
+         if( not isinstance(self._sour, int) or self._sour < 0 or self._sour > 255):
+            raise MAVlinkPlugException('Invalid header data : source : {0}'.format(self._sour))
+        #Type check
+        if( not self._type in MSG_PLUG_TYPE)): 
+            raise MAVlinkPlugException('Invalid header data : type : {0}'.format(self._type))
+
+    def set_data(self, data):
+        self._mav_msg_data
+    
+    def get_plug_msg_buf(self):
+        if(self._dest != None and self._sour != None and self._type != None):
+            header = struct.pack('!BBB', self._dest, self._sour, self._type)
+            msg = header + self._mav_msg_data
+            return msg
         else:
-            raise Exception()
-    
-    def decode(self, msgbuf):
+            raise MAVlinkPlugException('Header not defined')
+
+            
+class MAVlinkPlugMessage(PlugMessage):
+        def decode(self, msgbuf):
         '''decode a buffer as a MAVLink message'''
         # decode the header
         try:
             magic, mlen, seq, srcSystem, srcComponent, msgId = struct.unpack('cBBBBB', msgbuf[:6])
         except struct.error as emsg:
-            raise mavlink.MAVError('Unable to unpack MAVLink header: %s' % emsg)
+            raise MAVlinkPlugException('Unable to unpack MAVLink header: %s' % emsg)
         if ord(magic) != 254:
-            raise mavlink.MAVError("invalid MAVLink prefix '%s'" % magic)
+            raise MAVlinkPlugException("invalid MAVLink prefix '%s'" % magic)
         if mlen != len(msgbuf)-8:
-            raise mavlink.MAVError('invalid MAVLink message length. Got %u expected %u, msgId=%u' % (len(msgbuf)-8, mlen, msgId))
+            raise MAVlinkPlugException('invalid MAVLink message length. Got %u expected %u, msgId=%u' % (len(msgbuf)-8, mlen, msgId))
 
         if not msgId in mavlink.mavlink_map:
-            raise mavlink.MAVError('unknown MAVLink message ID %u' % msgId)
+            raise MAVlinkPlugException('unknown MAVLink message ID %u' % msgId)
 
         # decode the payload
         type = mavlink.mavlink_map[msgId]
@@ -72,17 +99,17 @@ class PlugMessage(object):
         try:
             crc, = struct.unpack('<H', msgbuf[-2:])
         except struct.error as emsg:
-            raise mavlink.MAVError('Unable to unpack MAVLink CRC: %s' % emsg)
+            raise MAVlinkPlugException('Unable to unpack MAVLink CRC: %s' % emsg)
         crcbuf = msgbuf[1:-2]
         crcbuf = crcbuf + struct.pack('B',crc_extra)
         crc2 = x25crc(crcbuf)
         if crc != crc2.crc:
-            raise mavlink.MAVError('invalid MAVLink CRC in msgID %u 0x%04x should be 0x%04x' % (msgId, crc, crc2.crc))
+            raise MAVlinkPlugException('invalid MAVLink CRC in msgID %u 0x%04x should be 0x%04x' % (msgId, crc, crc2.crc))
 
         try:
             t = struct.unpack(fmt, msgbuf[6:-2])
         except struct.error as emsg:
-            raise mavlink.MAVError('Unable to unpack MAVLink payload type=%s fmt=%s payloadLength=%u: %s' % (
+            raise MAVlinkPlugException('Unable to unpack MAVLink payload type=%s fmt=%s payloadLength=%u: %s' % (
                 type, fmt, len(msgbuf[6:-2]), emsg))
 
         tlist = list(t)
@@ -115,18 +142,10 @@ class PlugMessage(object):
         try:
             m = type(*t)
         except Exception as emsg:
-            raise mavlink.MAVError('Unable to instantiate MAVLink message of type %s : %s' % (type, emsg))
+            raise MAVlinkPlugException('Unable to instantiate MAVLink message of type %s : %s' % (type, emsg))
         m._msgbuf = msgbuf
         m._payload = msgbuf[6:-2]
         m._crc = crc
         m._header = mavlink.MAVLink_header(msgId, mlen, seq, srcSystem, srcComponent)
         self._mav_msg_data = m
         return m
-    
-    def get_plug_msg_buf(self):
-        if(self._dest != None and self._sour != None and _self._type != None):
-            header = struct.pack('BBB', self._dest, self._sour, self._type)
-            msg = header + self._mav_msg_buf
-            return msg
-        else:
-            raise Exception()
